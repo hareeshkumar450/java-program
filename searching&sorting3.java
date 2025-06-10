5072b7c29012d6436088b03fc8059b03f32fdc99
Hareesh kumar <hareeshkumar7929@gmail.com>
2025-06-10 14:28:26 +0530

    Add files via upload

---
+++ searching&sorting3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr=new int[5];
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<arr.length;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<arr.length-1;i++)
+		{
+		    for(int j=0;j<arr.length-1-i;i++)
+		    {
+		        if(arr[j]>arr[j+1])
+		        {
+		            int temp=arr[j];
+		            arr[j]=arr[j+1];
+		            arr[j+1]=temp;
+		        }
+		    }
+		}
+	}
+}
